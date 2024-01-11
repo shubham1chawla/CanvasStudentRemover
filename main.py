845fc550c933c20261f51a51f518c2efdd54c0d7
Shubham Chawla <shubham_chawla@live.com>
2024-01-11 17:59:47 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from argparse import ArgumentParser
+
+TIMEOUT_SECONDS = 60
+
+parser = ArgumentParser(description="Removes Students from Canvas course automatically.")
+parser.add_argument("--url", type=str, help="Link to the people's tab of the Canvas course, Eg. https://canvas.asu.edu/courses/<code>/users")
+parser.add_argument("--username", type=str, help="ASU login username")
+parser.add_argument("--password", type=str, help="ASU login password")
+parser.add_argument("--limit", type=int, help="Limit student removal", default=25)
+parser.add_argument("--exclude", type=str, help="Path to exclude file")
+args = parser.parse_args()
+
+
+def handle_duo_login() -> None:
+    duo_iframe = driver.find_element(By.CSS_SELECTOR, "#login > iframe")
+    driver.switch_to.frame(duo_iframe)
+
+    wait(driver, TIMEOUT_SECONDS).until(EC.presence_of_element_located((By.ID, "auth_methods")))
+    push_option = driver.find_element(By.CLASS_NAME, "push-label")
+    push_option.find_element(By.TAG_NAME, "button").click()
+
+    driver.switch_to.parent_frame()
+
+
+def handle_asu_login() -> None:
+    username_input = driver.find_element(By.ID, "username")
+    username_input.send_keys(args.username)
+
+    password_input = driver.find_element(By.ID, "password")
+    password_input.send_keys(args.password)
+
+    submit_button = driver.find_element(By.NAME, "submit")
+    submit_button.click()
+
+
+def wait_for_loading_students() -> None:
+    def is_loading() -> bool:
+        input = driver.find_element(By.CSS_SELECTOR, "#tab-0 > input[type=hidden]")
+        return input.get_attribute("class") == "loading"
+    wait(driver, TIMEOUT_SECONDS).until(lambda _: not is_loading())
+
+
+def apply_filter(contains: str) -> None:
+    wait(driver, TIMEOUT_SECONDS).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#tab-0 > select")))
+    select = driver.find_element(By.CSS_SELECTOR, "#tab-0 > select")
+    for option in select.find_elements(By.TAG_NAME, "option"):
+        if contains in option.get_attribute("innerHTML"):
+            value = option.get_attribute("value")
+            Select(select).select_by_value(value)
+            break
+    wait_for_loading_students()
+
+
+def remove_students() -> None:
+    exclude_names = set()
+    with open(args.exclude) as file:
+        for exclude_name in file.read().splitlines():
+            exclude_names.add(exclude_name.strip())
+    print(f"Excluding: {exclude_names}")
+
+    removed = 0
+    while removed < args.limit:
+        apply_filter("Student")
+
+        students = driver.find_elements(By.XPATH, "//tr[contains(@class, 'rosterUser')]")
+        student = None
+        for possible_student in students:
+            student_name = possible_student.find_element(By.CSS_SELECTOR, "td > a").get_attribute("innerHTML")
+            if student_name.strip() not in exclude_names:
+                print(f"{removed+1} - REMOVING: {student_name}")
+                student = possible_student
+                break
+        if not student:
+            print(f"No student found! Removed {removed} students.")
+            break
+
+        wait(driver, TIMEOUT_SECONDS).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "td.right > div.admin-links > a")))
+        student.find_element(By.CSS_SELECTOR, "td.right > div.admin-links > a").click()
+
+        wait(driver, TIMEOUT_SECONDS).until(EC.element_to_be_clickable(
+            (By.XPATH, "//td[contains(@class, 'right')][.//a[text()[contains(., 'Remove From Course')]]]")
+        ))
+        admin_links = student.find_elements(By.CSS_SELECTOR, "td.right > div.admin-links > ul > li > a")
+        for admin_link in admin_links:
+            if "Remove From Course" in admin_link.get_attribute("innerHTML"):
+                admin_link.click()
+                break
+
+        wait(driver, TIMEOUT_SECONDS).until(EC.alert_is_present())
+        driver.switch_to.alert.accept()
+        driver.switch_to.parent_frame()
+        removed += 1
+
+        apply_filter("All Roles")
+
+
+# Setting up Chrome
+options = Options()
+options.add_experimental_option("detach", True)
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get(args.url)
+driver.maximize_window()
+
+handle_asu_login()
+handle_duo_login()
+remove_students()
